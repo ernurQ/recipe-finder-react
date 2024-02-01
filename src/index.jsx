6a01c0439d9448072c2fadc727f3b1235c022d37
ernur <220776@astanait.edu.kz>
2024-02-01 11:59:41 +0600

    created project

---
+++ src/index.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import App from './components/app/App';
+
+import 'react-bootstrap-typeahead/css/Typeahead.css'
+import 'bootstrap/dist/css/bootstrap.min.css'
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+    <App />
+)
